3f7d3bbb721b5a865f514618d87476bf17d989df
Fred Shepherd <fmbshepherd@gmail.com>
2019-11-26 18:26:23 +0100

    burger

---
+++ components/Burger.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function Burger() {
+  return (
+    <svg width="34" height="32" viewBox="0 0 34 32">
+      <line
+        x1="0.888916"
+        y1="2.14444"
+        x2="33.1111"
+        y2="2.14444"
+        stroke="#111111"
+        strokeWidth="4"
+      />
+      <line
+        x1="0.888916"
+        y1="16"
+        x2="33.1111"
+        y2="16"
+        stroke="#001F3F"
+        strokeWidth="4"
+      />
+      <line
+        x1="0.888916"
+        y1="29.8556"
+        x2="33.1111"
+        y2="29.8556"
+        stroke="#111111"
+        strokeWidth="4"
+      />
+    </svg>
+  )
+}
