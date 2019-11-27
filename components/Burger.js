00aaba38bb1986e2a5e69b54041524b79959a752
Fred Shepherd <fmbshepherd@gmail.com>
2019-11-27 11:13:22 +0100

    add cross

--- components/Burger.js
+++ components/Burger.js
@@ -1,7 +1,26 @@
 import React from "react"
 
-export default function Burger() {
-  return (
+export default function Burger(props) {
+  return props.active ? (
+    <svg width="36" height="35" viewBox="0 0 36 35" fill="none">
+      <line
+        x1="2.40677"
+        y1="33.4208"
+        x2="32.7179"
+        y2="2.07634"
+        stroke="#111111"
+        strokeWidth="4"
+      />
+      <line
+        x1="3.19205"
+        y1="2.05246"
+        x2="34.5365"
+        y2="33.3969"
+        stroke="#111111"
+        strokeWidth="4"
+      />
+    </svg>
+  ) : (
     <svg width="34" height="32" viewBox="0 0 34 32">
       <line
         x1="0.888916"
